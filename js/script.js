02804c9294de6f11d527950847412b29dd0fefee
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-03-06 09:12:39 -0300

    att modal

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function opcoesPizza() {
   const section = document.querySelector("section");
   section.innerHTML = "";
 
-  pizzas.forEach((pizza) => {
+  pizzas.map((pizza) => {
     const divPizza = document.createElement("div");
     divPizza.id = "pizza";
     divPizza.classList.add(pizza.id);
@@ -14,25 +14,26 @@ function opcoesPizza() {
     <p>Tamanho ${pizza.tamanho[0]}</p>
     <p>A partir de R$${pizza.preco[0]}</p>
     <p>${pizza.descricao}</p>
-    <button class="btn-vermelho" id="adicionar">Adicionar</button>
+    <button class="btn-vermelho" id="adicionar"">Adicionar</button>
     `;
     section.appendChild(divPizza);
+
+    const botaoAdicionar = divPizza.querySelector("#adicionar");
+    botaoAdicionar.addEventListener("click", () => {
+      exibirInformacoesPizza(pizza);
+    });
   });
 }
 
 opcoesPizza();
 
-function exibirInformacoesPizza() {
+function exibirInformacoesPizza(pizza) {
   const divEscolha = document.getElementById("escolha");
-  const botaoAdd = document.getElementById("adicionar");
   const menu = document.getElementById("menu");
 
-  botaoAdd.addEventListener("click", () => {
-    divEscolha.style.display = "grid";
-    divEscolha.innerHTML = "";
+  divEscolha.style.display = "grid";
 
-    pizzas.map((pizza) => {
-      divEscolha.innerHTML = `
+  divEscolha.innerHTML = `
       <img id="imagemPizza" src="${pizza.imagem}" />
       <div id="descricao">
       <h2 id="nomePizza">${pizza.nome}</h2>
@@ -42,37 +43,34 @@ function exibirInformacoesPizza() {
       <button id="botaoP" class="botaoTamanho">${pizza.tamanho[0]}</button>
       <button id="botaoM" class="botaoTamanho">${pizza.tamanho[1]}</button>
       <button id="botaoG" class="botaoTamanho">${pizza.tamanho[2]}</button></p>
-      <p id="precoPizza" onclick="selecionarTamanho()">Selecione o tamanho para visualisar o preço</p>
+      <p id="precoPizza" onclick="selecionarTamanho()">Selecione o tamanho</p>
       <p id="descricaoPizza">${pizza.descricao}</p>
       <button id="botaoCompra" class="btn-vermelho">Comprar</button>
       <button id="botaoCancelar">Cancelar</button>
       </div>
       `;
-    });
-    menu.style.opacity = "0.5";
+  menu.style.opacity = "0.5";
 
-    selecionarTamanho();
-  });
+  selecionarTamanho(pizza);
 }
 
-function selecionarTamanho() {
+function selecionarTamanho(pizza) {
   const divEscolha = document.getElementById("escolha");
   const precoPizza = document.getElementById("precoPizza");
   const botaoCancelar = document.getElementById("botaoCancelar");
+  const menu = document.getElementById("menu");
 
-  pizzas.forEach((pizza) => {
-    const botaoP = document.getElementById("botaoP");
-    botaoP.addEventListener("click", () => {
-      precoPizza.innerHTML = `Preço: R$ ${pizza.preco[0].toFixed(2)}`;
-    });
-    const botaoM = document.getElementById("botaoM");
-    botaoM.addEventListener("click", () => {
-      precoPizza.innerHTML = `Preço: R$ ${pizza.preco[1].toFixed(2)}`;
-    });
-    const botaoG = document.getElementById("botaoG");
-    botaoG.addEventListener("click", () => {
-      precoPizza.innerHTML = `Preço: R$ ${pizza.preco[2].toFixed(2)}`;
-    });
+  const botaoP = document.getElementById("botaoP");
+  botaoP.addEventListener("click", () => {
+    precoPizza.innerHTML = `Preço: R$ ${pizza.preco[0].toFixed(2)}`;
+  });
+  const botaoM = document.getElementById("botaoM");
+  botaoM.addEventListener("click", () => {
+    precoPizza.innerHTML = `Preço: R$ ${pizza.preco[1].toFixed(2)}`;
+  });
+  const botaoG = document.getElementById("botaoG");
+  botaoG.addEventListener("click", () => {
+    precoPizza.innerHTML = `Preço: R$ ${pizza.preco[2].toFixed(2)}`;
   });
 
   botaoCancelar.addEventListener("click", () => {
@@ -80,5 +78,3 @@ function selecionarTamanho() {
     menu.style.opacity = "1";
   });
 }
-
-exibirInformacoesPizza();
